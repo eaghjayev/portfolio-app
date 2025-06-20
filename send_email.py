1f46fb4efc9e07fccb4c79a51b0ddc6f3dae46c3
eaghjayev <eaghjayev2018@outlook.com>
2025-06-20 12:07:03 +0400

    Day 23: Set environment variables

--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
-import smtplib
-import ssl
+import smtplib, ssl
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "eaghjayev96@gmail.com"
-    password = "ouot qqyj bzpa bgqp"
+    password = os.getenv("MAIL_PASSWORD")
 
     receiver = "eaghjayev96@gmail.com"
     context = ssl.create_default_context()
